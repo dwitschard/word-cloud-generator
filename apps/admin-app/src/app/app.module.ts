27104f6cd1447baf2777748f20ee9f0e08781566
bibo <bill.bosshard@zuehlke.com>
2021-09-01 09:37:31 +0200

    word form firestore persistence layer

--- apps/admin-app/src/app/app.module.ts
+++ apps/admin-app/src/app/app.module.ts
@@ -3,9 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import {WordFormComponent} from "./word-form/word-form.component";
+import { ReactiveFormsModule } from "@angular/forms";
+
+import { environment } from '../environments/environment';
+import {AngularFireModule} from "@angular/fire/compat";
+import {AngularFirestoreModule} from "@angular/fire/compat/firestore";
+import {WordsService} from "./words.service";
+// import {WordListComponent} from "./word-list/word-list.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, WordFormComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
@@ -13,9 +21,12 @@ import { RouterModule } from '@angular/router';
         path: '',
         loadChildren: () => import('./remote-entry/entry.module').then((m) => m.RemoteEntryModule),
       },
-    ], { initialNavigation: 'enabledBlocking' }),
+    ], {initialNavigation: 'enabledBlocking'}),
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule
   ],
-  providers: [],
+  providers: [WordsService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
