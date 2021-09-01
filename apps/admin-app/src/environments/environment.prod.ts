ab3437295537c2df1b2a36b312113854303163b0
bibo <bill.bosshard@zuehlke.com>
2021-09-01 09:52:48 +0200

    env

--- apps/admin-app/src/environments/environment.prod.ts
+++ apps/admin-app/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
   production: true,
+  firebaseConfig : {
+    apiKey: "",
+    authDomain: "jap-wordcloud.firebaseapp.com",
+    databaseURL: "https://jap-wordcloud-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "jap-wordcloud",
+    storageBucket: "jap-wordcloud.appspot.com",
+    messagingSenderId: "941959563643",
+    appId: "1:941959563643:web:abdd242a601e7d5a3c55de",
+    measurementId: "G-9HC598C5BR"
+  }
 };
--- apps/admin-app/src/environments/environment.ts
+++ apps/admin-app/src/environments/environment.ts
@@ -4,6 +4,16 @@
 
 export const environment = {
   production: false,
+  firebaseConfig : {
+    apiKey: "",
+    authDomain: "jap-wordcloud.firebaseapp.com",
+    databaseURL: "https://jap-wordcloud-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "jap-wordcloud",
+    storageBucket: "jap-wordcloud.appspot.com",
+    messagingSenderId: "941959563643",
+    appId: "1:941959563643:web:abdd242a601e7d5a3c55de",
+    measurementId: "G-9HC598C5BR"
+  }
 };
 
 /*
